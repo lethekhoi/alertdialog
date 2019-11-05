f9aa2e006d69b6084c9014aa2fa6402c7d875e8b
lethekhoi <lethekhoi@gmail.com>
2019-11-05 21:33:45 +0700

    alert dialog

--- app/src/main/java/lethekhoi/ltk/hcmus/alertdialog/MainActivity.java
+++ app/src/main/java/lethekhoi/ltk/hcmus/alertdialog/MainActivity.java
@@ -1,7 +1,9 @@
 package lethekhoi.ltk.hcmus.alertdialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,7 +27,22 @@ public class MainActivity extends AppCompatActivity {
         mBtnDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Hop thoai xac nhan tat appp")
+                        .setMessage("Ban hay xac nhan ben duoi !!!")
+                        .setPositiveButton("Co", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();
+                            }
+                        })
+                .setNegativeButton("Khong", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                builder.show();
             }
         });
     }
