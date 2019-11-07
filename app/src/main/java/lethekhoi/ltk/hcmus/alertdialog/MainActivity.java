6b27bcd6d6d21ce2a4edae6d434819468eedb3e5
lethekhoi <lethekhoi@gmail.com>
2019-11-07 20:22:20 +0700

    single choise

--- app/src/main/java/lethekhoi/ltk/hcmus/alertdialog/MainActivity.java
+++ app/src/main/java/lethekhoi/ltk/hcmus/alertdialog/MainActivity.java
@@ -1,12 +1,15 @@
 package lethekhoi.ltk.hcmus.alertdialog;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button mBtnDialog;
@@ -16,34 +19,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mBtnDialog = findViewById(R.id.buttonAlertDialog);
-    //    Banhmi banhmi = new Banhmi("tron", "salad son", "ca chua bi", "sot bo", "pepsi");
-        Banhmi banhmi= new BanhmiFastFood()
-                .loai("que")
-                .cachua("Ca chua bi")
-                .nuocuong("pepsi")
-                .build();
+        //ctrl +. => chọn remove
+        //    Banhmi banhmi = new Banhmi("tron", "salad son", "ca chua bi", "sot bo", "pepsi");
+//        Banhmi banhmi= new BanhmiFastFood()
+//                .loai("que")
+//                .cachua("Ca chua bi")
+//                .nuocuong("pepsi")
+//                .build();
         //1 :  cac chuc nang set cho o banh mi
         //2 : build o banh mi voi cac chưc năng muôn set lai
+//        mBtnDialog.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+//                builder.setTitle("Hop thoai xac nhan tat appp");
+//                builder.setMessage("Ban hay xac nhan ben duoi !!!");
+//                builder.setPositiveButton("Co", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                finish();
+//                            }
+//                        })
+//                .setNegativeButton("Khong", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        dialogInterface.cancel();
+//
+//                    }
+//                });
+//
+//                builder.show();
+//            }
+//        });
         mBtnDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Context context;
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Hop thoai xac nhan tat appp")
-                        .setMessage("Ban hay xac nhan ben duoi !!!")
-                        .setPositiveButton("Co", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                finish();
-                            }
-                        })
-                .setNegativeButton("Khong", new DialogInterface.OnClickListener() {
+                builder.setTitle("Chọn ngôn ngữ sử dụng");
+//                builder.setMessage("Các ngôn ngữ thông dụng");
+
+                final String[] arrayLang = {"C", "C++", "Php", "JavaScript", "Scala"};
+                builder.setSingleChoiceItems(arrayLang, -1, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.cancel();
+                        Toast.makeText(MainActivity.this, arrayLang[i], Toast.LENGTH_SHORT).show();
+                        dialogInterface.dismiss();
                     }
                 });
-                builder.show();
+builder.show();
+
+                //annotation
+//                showToast(new Object());
+//                builder.show();
             }
-        });
+
+//            @Deprecated
+//            public void showToast(@NonNull String text) {
+//            }
+//
+//            public void showToast(Object text) {
+//            }
+
+            });
+        }
     }
-}
